722a931aabdfd114d44cc86c61b5417982f2a1e1
Mridul Gain <mridulgain@gmail.com>
2019-08-12 13:03:56 +0530

    coding done

--- graph_analysis.py
+++ graph_analysis.py
@@ -1,6 +1,13 @@
 import sys
+import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
+from scipy.special import zetac
+from scipy.optimize import curve_fit
+
+def fit_func(x,a):
+	 return (x**-a)/zetac(a)
+
 def degree_distribution(g):
 	dd = {}
 	for n in g.nodes():
@@ -9,14 +16,21 @@ def degree_distribution(g):
 			dd[deg] = 0
 		dd[deg] += 1
 	deg_count = sorted(dd.items ())
-	fig = plt.figure ()
-	ax = fig.add_subplot (111)
 	k = [deg for (deg,count) in deg_count]
 	no_of_nodes = len(g.nodes())
 	pk = [count/no_of_nodes for (deg, count) in deg_count]
-	ax.plot(k, pk)
-	plt.title("Degree  Distribution")
-	#fig.savefig("degree_distribution.png")
+	popt, pcov = curve_fit(fit_func, k, pk)
+	perr = np.sqrt(np.diag(pcov))
+	print("Power law exponent : %5.5f (with standard deviation error : %5.5f)" %(popt, perr))
+	# visualisation
+	plt.plot(k, pk, 'b-', label='data')
+	plt.plot(k, fit_func(k, *popt), 'r-', label='fit: alpha = %5.3f \nstandard deviation errr = %5.5f' %(popt, perr))
+	plt.xlabel('k')
+	plt.ylabel('p(k)')
+	title = "Degree  Distribution" + " for <%s>" % (sys.argv[1].split('.')[0])
+	plt.title(title)
+	plt.legend()
+	plt.savefig(title + ".png")
 	plt.show()
 
 def analyse_graph(G):
@@ -30,15 +44,15 @@ def analyse_graph(G):
 	# avg degree
 	no_of_nodes = len(G.nodes())
 	total_degree = sum(deg_list)
-	print("Average degree : ", total_degree/no_of_nodes)#avg degree
+	print("Average degree : %5.5f" %(total_degree/no_of_nodes))#avg degree
 	# clustering coefficient
 	total_clustering_coeff = sum(nx.clustering(G).values())
-	print("Average clustering coefficient : ", total_clustering_coeff/no_of_nodes)#avg clustering coeff
+	print("Average clustering coefficient : %5.5f" %(total_clustering_coeff/no_of_nodes))#avg clustering coeff
 	# degree distrib 
 	degree_distribution(G) # degree_distribution
 	# avg path length and diameter
 	try:
-		print("Average path length : ", nx.average_shortest_path_length(G))
+		print("Average path length : %5.5f" %(nx.average_shortest_path_length(G)) )
 		print("Diameter : ", nx.diameter(G))
 	except nx.NetworkXError:
 		print("Graph is not connected. So computing average path length and diameter for its largest connected component ---")
@@ -47,7 +61,7 @@ def analyse_graph(G):
 		for C in nx.connected_component_subgraphs(G):
 			shortest_path_len.append(nx.average_shortest_path_length(C))
 			diam.append(nx.diameter(C))
-		print("Avg path length : ", max(shortest_path_len))
+		print("Avg path length : %5.5f" %(max(shortest_path_len)) )
 		print("Diameter : ", max(diam))
 
 if __name__ == '__main__':
@@ -55,4 +69,9 @@ if __name__ == '__main__':
 		G = nx.read_edgelist(sys.argv[1])
 		analyse_graph(G)
 	except FileNotFoundError:
-		print("Check the file name you entered")
+		print("Check the file name you have entered")
+	except IndexError:
+		print("Enter input file name as command line argument")
+		print("Usage : python graph_analysis.py file_name")
+	except TypeError:
+		print("You should enter your graph in EDGE LIST format")
--- test_plt.py
+++
@@ -1,8 +0,0 @@
-import networkx as nx
-G = nx.read_edgelist('com-youtube.ungraph.txt')
-try:
-	diam = nx.diameter(G)
-	print(diam)
-except nx.NetworkXError:
-	for C in nx.connected_component_subgraphs(G):
-	    print(nx.diameter(C))
